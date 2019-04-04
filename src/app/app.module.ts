06447790db56828f70b38da1775a7022671b703e
Marc Bernini <bernini.marc@gmail.com>
2019-04-04 19:06:23 +0200

    tabsCountdown with two tabs on desktop and tablets devices. One tab on smartphone.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsCountdownComponent } from './tabs-countdown/tabs-countdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsCountdownComponent
   ],
   imports: [
     BrowserModule,
