13e8bcde4650fd699bba5821ed7a0b06a2244f3b
Sarawi Long <long.sarawi@gmail.com>
2019-04-08 10:57:43 +0200

    footer fixed with position absolute still need to work out for the responsive mobile and creation of the homepage router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabsCountdownComponent } from './tabs-countdown/tabs-countdown.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {
-    path : 'home',
-    component : TabsCountdownComponent
+    path : '',
+    pathMatch: 'full',
+    component : HomepageComponent
   }
 ];
 
