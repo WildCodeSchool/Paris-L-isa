d72cf18393b977f912a82c678b8d1b52c1cb0eda
Julia Hu <juliahukoshi@gmail.com>
2019-04-08 11:30:44 +0200

    created calendar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabsCountdownComponent } from './tabs-countdown/tabs-countdown.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   {
     path : 'home',
     component : TabsCountdownComponent
-  }
+  },
+  {
+    path : 'agenda',
+    component : CalendarComponent
+  },
 ];
 
 @NgModule({
