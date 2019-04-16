e0720def17907c6d574fb5c8085769e92868175d
Triple <sebastien.andrys@gmail.com>
2019-04-16 11:54:23 +0200

    branch frenchMap merge de dev sur frenchMap to be update

---
+++ src/app/pages/astro/astro.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-astro',
+  templateUrl: './astro.component.html',
+  styleUrls: ['./astro.component.scss']
+})
+export class AstroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+}
