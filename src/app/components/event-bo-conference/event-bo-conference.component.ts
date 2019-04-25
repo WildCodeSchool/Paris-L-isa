209365edbf914dac13093288e016f92e4d67f2bd
paul charensol <paulcharensol@Air-de-Ghalia.localdomain>
2019-04-25 18:12:59 +0200

    wip

---
+++ src/app/components/event-bo-conference/event-bo-conference.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-bo-conference',
+  templateUrl: './event-bo-conference.component.html',
+  styleUrls: ['./event-bo-conference.component.scss']
+})
+export class EventBoConferenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/event-bo-sky-event/event-bo-sky-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-bo-sky-event',
+  templateUrl: './event-bo-sky-event.component.html',
+  styleUrls: ['./event-bo-sky-event.component.scss']
+})
+export class EventBoSkyEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
