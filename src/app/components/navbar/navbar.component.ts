e2c07dea451be49e384cbfc3802c1bcaa352969f
helderscrolls <nortehelder@gmail.com>
2019-04-30 18:04:49 +0200

    added scroll background color change effect, fixed searchbar size on mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import { HostListener, Inject } from "@angular/core";
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  changeColor: boolean;
+
 
   @ViewChild('navBurger') navBurger: ElementRef;
   @ViewChild('navMenu') navMenu: ElementRef;
@@ -15,6 +18,17 @@ export class NavbarComponent {
     this.navMenu.nativeElement.classList.toggle('is-active');
   }
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number < 25) {
+      this.changeColor = false;
+      
+    } else {
+      this.changeColor = true;
+    }
+  }
 }
 
 
